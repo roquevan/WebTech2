3c0dfa16c929f5c90e06e34c1b67afdc96ba4dc3
Kurt Vonn Alde <kurt.alde@lsu.edu.ph>
2021-02-20 19:58:07 +0800

    Updated

--- indexx.ts
+++ indexx.ts
@@ -3,5 +3,5 @@ var grade1 = 87;
 var grade2 = 93;
 var grade3 = 86
 var ave = grade1 + grade2 + grade3 / 3;
-console.log(name + "Your final grade is" + ave);
+console.log(fullname + "Your final grade is" + ave);
 

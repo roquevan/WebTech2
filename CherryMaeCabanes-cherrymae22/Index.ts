9ad62819075a468ffae2f80cec4f7b8e80cb2273
cherrymae22 <cherrychu611@gmail.com>
2021-02-22 19:26:52 +0800

    initial commit

---
+++ CherryMaeCabanes-cherrymae22/Index.ts
@@ -0,0 +1,9 @@
+var name1:string = "John"; 
+var score1:number = 50;
+var score2:number = 42.50
+var sum = score1 + score2 
+console.log("Hello")
+console.log("Name"+name1) 
+console.log("First Score: "+score1) 
+console.log("Second Score: "+score2) 
+console.log("Sum of the Scores: "+sum)

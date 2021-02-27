c34e84c477a63cdecc0b7e0bd76ff16b90695022
Tasi101 <rhealou.neri@lsu.edu.ph>
2021-02-27 21:30:58 +0800

    initial commit

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World")
+var word :string = "Hello World"; 
+var word1:string = "World";
+var word2:string= " World"
+var sum =word1 + word2 
+console.log("name"+name) 
+console.log("first score: "+word1) 
+console.log("second score: "+word2) 
+console.log("sum of the scores: "+sum)
